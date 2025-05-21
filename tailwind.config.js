5a15e7888f8f16702ad8616d59a0367d9a2ece37
Ayush-alt-sys <igayushacharya@gmail.com>
2025-05-22 01:45:03 +0530

    installed the tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js as ES module (optional)
+export default {
+  content: ["./src/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        mycustomcolor: '#123456',
+      },
+    },
+  },
+  plugins: [],
+}
