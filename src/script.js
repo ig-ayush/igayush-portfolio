6cba14413e35691a7fec9195eef0385ebec5971f
Ayush-alt-sys <igayushacharya@gmail.com>
2025-05-24 00:29:27 +0530

    Finish 80% of responsive of the home page

--- src/script.js
+++ src/script.js
@@ -1,3 +1,13 @@
-document.getElementById("aboutmeBtn").addEventListener("click",()=>{
-    console.log("Clicked");
-})
+const ProfilePics = [
+  "./Assets/Images/pic-1.jpg",
+  "./Assets/Images/pic-2.jpg",
+  "./Assets/Images/pic-3.jpg",
+  "./Assets/Images/pic-4.jpg",
+];
+
+const PresentPics = document.getElementById("profile-img");
+let i = 0;
+  setInterval(() => {
+    i = (i + 1) % ProfilePics.length;
+    PresentPics.src = ProfilePics[i];
+  }, 3000);
