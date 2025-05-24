f576927b45869dba2f336be5f05c1a3b56143d52
Ayush-alt-sys <igayushacharya@gmail.com>
2025-05-24 23:25:24 +0530

    added the new text feture

--- src/script.js
+++ src/script.js
@@ -7,7 +7,44 @@ const ProfilePics = [
 
 const PresentPics = document.getElementById("profile-img");
 let i = 0;
-  setInterval(() => {
-    i = (i + 1) % ProfilePics.length;
-    PresentPics.src = ProfilePics[i];
-  }, 3000);
+setInterval(() => {
+  i = (i + 1) % ProfilePics.length;
+  PresentPics.src = ProfilePics[i];
+}, 3000);
+
+const buildTexts = document.getElementById("tech");
+
+const tech = ["Apps", "Websites", "Frontends", "Designs"];
+
+let currentText = 0;
+
+function changeTech() {
+  let oldText = tech[currentText];
+  currentText = (currentText + 1) % tech.length;
+  let newText = tech[currentText];
+
+  animationText(oldText, newText);
+}
+
+function animationText(oldText, newText) {
+  let maxLen = Math.max(oldText.length, newText.length);
+  buildTexts.textContent = "";
+
+  for (let i = 0; i < maxLen; i++) {
+    console.log("hwllo");
+    let oldChar = oldText[i] || "";
+    let newChar = newText[i] || "";
+    if (oldChar !== newChar) {
+      setTimeout(() => {
+        buildTexts.classList.add("out");
+        setTimeout(() => {
+          buildTexts.textContent += newChar;
+          buildTexts.classList.remove("out");
+          buildTexts.classList.add("in");
+        }, 150);
+      }, i * 100);
+    }
+  }
+}
+
+setInterval(changeTech,3000);
